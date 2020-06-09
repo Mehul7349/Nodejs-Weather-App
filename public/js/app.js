379fd6e662db858e38429be61957c5233e25441c
Mehul Varshney <mehul@cerebry.co>
2020-06-09 08:13:28 +0530

    ready for heroku

--- public/js/app.js
+++ public/js/app.js
@@ -11,7 +11,7 @@ weatherForm.addEventListener('submit',(event) => {
     msgOne.textContent = 'Loading...'
     msgTwo.textContent = ''
 
-    const url='http://localhost:3000/weather?address='+location
+    const url='/weather?address='+location
     fetch(url).then((response) => {
         response.json().then((data) => {
             if(data.error){
